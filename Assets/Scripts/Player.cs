0588a928b9325c41c2d1e42304a4484feb584b01
unknown <tylersnapier@gmail.com>
2022-08-12 15:24:55 -0400

    Cleaned up data and added space key functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,35 @@ public class Player : MonoBehaviour
     [SerializeField] 
     private float _speed = 3.5f;
     
-    // Start is called before the first frame update
+  
     void Start()
     {
-       //take the current position = new position (0, 0, 0)
        transform.position = new Vector3(0, 0, 0);  
     }
 
-    // Update is called once per frame
+    
     void Update()
     {  
+
+        CalculateMovement();
+
+        //if i hit the space key
+        //spawn gameObject
+    
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space Key Pressed");
+        }
+        
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
 
         float verticalInput = Input.GetAxis("Vertical");    
         
-        //new Vector3(-5, 0, 0) * 5 * real time
+        
         transform.Translate(Vector3.left * horizontalInput * _speed * Time.deltaTime);
 
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
@@ -37,10 +51,7 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, -3.8f, 0);
 
         }
-        //if player on the x > 11
-        //x pos = -11
-        //else if player on the x is less than -11
-        //x pos = 11
+        
 
         if (transform.position.x > 11.3f)
         {
@@ -50,5 +61,5 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
-        }
+    }    
 }
