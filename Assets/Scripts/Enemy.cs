8931aac271a00e7ed95452793907571db9c2e4d0
unknown <tylersnapier@gmail.com>
2022-08-30 04:29:58 -0400

    Added Enemy Prefabs and Enemy Behavior

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     //move down at 4 meters per second
+    transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+    //if bottom of screen
+    //respawn at top with a new random x position
+
+    if (transform.position.y < -5f)
+     {
+     float randomX = Random.Range(-8f, 8f);
+     transform.position = new Vector3(randomX, 7, 0);
+     }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       //if other is Player
+       //damage the player
+       //Destroy Us
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+       //if other is laser
+       //laser
+       //destroy us
+
+       if (other.tag == "Laser")
+       {
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+       }
+    }
+}
