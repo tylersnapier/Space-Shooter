cae893590c6e2c1998da6471007e222b30c1425d
unknown <tylersnapier@gmail.com>
2022-08-31 08:23:52 -0400

    Added Spawn Manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //spawn game objects every 5 seconds
+    //Create a coroutine of type IEnumerator -- Yield Events
+    //while loop
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+        //while loop (infinite loop)
+            //Instantiate enemy prefab
+            //yield wait for 5 seconds
+    }
+}
