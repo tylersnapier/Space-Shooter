e88defde00a50a19eac4504ea2abd77a57e5af53
Alaric Alabaster <tylersnapier@gmail.com>
2024-05-12 16:57:14 -0400

    Created new enemy type

--- Assets/Scripts/Enemy_Flank_Laser.cs
+++ Assets/Scripts/Enemy_Flank_Laser.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Enemy_Flank_Laser : MonoBehaviour
 {
-
+    [SerializeField]
     private float _speed = 3.0f;
+    private GameObject _horizontalLaser;
+    private float _fireRate = 1f;
+    private float _canFire = -1f;
+
+    [SerializeField]
+    private bool _alive = true;
+
+    private Player _player;
+    private Animator _anim;
+    private AudioSource _audioSource;
 
     // Start is called before the first frame update
     void Start()
